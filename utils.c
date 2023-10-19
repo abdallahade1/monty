8eabad7685f6e00bde4254babcacb7832b83328a
abdallahade1 <abdoadelshabaan@gmail.com>
2023-10-19 14:57:15 -0700

    utils.c

--- utils.c
+++ utils.c
@@ -1,4 +1,21 @@
 #include "monty.h"
+
+/**
+ * free_stack - free a list
+ * @head: pointer to first node
+ *
+ */
+void free_stack(stack_t *head)
+{
+        stack_t *tmp;
+
+        while (head != NULL)
+        {
+                tmp = head->next;
+                free(head);
+                head = tmp;
+        }
+}
 /**
  * exit_error - frees the stack and exits due to erro
  * @stack: pointer to the head of the stack
